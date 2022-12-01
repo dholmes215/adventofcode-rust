fb57421be8a3d1bcfc6a7ed0d4809bff6bd8f528
David Holmes <dholmes@dholmes.us>
2022-12-01 17:55:30 -0600

    Add copyright notices

--- src/year2021/day01.rs
+++ src/year2021/day01.rs
@@ -1,3 +1,10 @@
+//
+// Copyright (c) 2022 David Holmes (dholmes at dholmes dot us)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
 use adventofcode_rust::aoc::*;
 
 fn parse_input(input: &str) -> Vec<i32> {
