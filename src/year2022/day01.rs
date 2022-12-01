ff82df6fac4c29118bec6366aa7c4a2a9a0a9a8b
David Holmes <dholmes@dholmes.us>
2022-12-01 17:24:29 -0600

    [2022-01] Improvements

--- src/year2022/day01.rs
+++ src/year2022/day01.rs
@@ -3,19 +3,16 @@ use adventofcode_rust::aoc::*;
 pub fn day01(input: &str) -> SolutionResult {
     let input_lines = input.split("\n");
 
-    let mut elf_snacks: Vec<Vec<i32>> = vec!(vec!());
+    let mut elf_calories: Vec<i32> = vec!(0);
     for line in input_lines {
         if line.is_empty() {
-            elf_snacks.push(vec!());
+            elf_calories.push(0);
         } else {
             let snack = line.parse::<i32>().unwrap();
-            let mut elf = elf_snacks.pop().unwrap();
-            elf.push(snack);
-            elf_snacks.push(elf);
+            *elf_calories.last_mut().unwrap() += snack;
         }
     }
 
-    let mut elf_calories = elf_snacks.iter().map(|list| list.iter().sum()).collect::<Vec<i32>>();
     elf_calories.select_nth_unstable_by(2, |a,b| b.cmp(a));
     elf_calories.resize(3, 0);
 
