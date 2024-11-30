1bc7ab4ecf6ba5583423c7e8894804871f0df48a
David Holmes <dholmes@dholmes.us>
2024-11-30 16:19:43 -0600

    Strip carriage returns from inputs

--- src/main.rs
+++ src/main.rs
@@ -12,35 +12,35 @@ mod year2022;
 
 fn main() {
     {
-        let input = fs::read_to_string("data/2021/01.txt").unwrap();
+        let input = fs::read_to_string("data/2021/01.txt").unwrap().replace("\r", "");
         let result = year2021::day01::day01(&input);
         std::println!("2021 Day 01 Part 1: {}", result.a);
         std::println!("2021 Day 01 Part 2: {}", result.b);
     }
 
     {
-        let input = fs::read_to_string("data/2022/01.txt").unwrap();
+        let input = fs::read_to_string("data/2022/01.txt").unwrap().replace("\r", "");
         let result = year2022::day01::day01(&input);
         std::println!("2022 Day 01 Part 1: {}", result.a);
         std::println!("2022 Day 01 Part 2: {}", result.b);
     }
 
     {
-        let input = fs::read_to_string("data/2022/03.txt").unwrap();
+        let input = fs::read_to_string("data/2022/03.txt").unwrap().replace("\r", "");
         let result = year2022::day03::day03(&input);
         std::println!("2022 Day 03 Part 1: {}", result.a);
         std::println!("2022 Day 03 Part 2: {}", result.b);
     }
 
     {
-        let input = fs::read_to_string("data/2022/04.txt").unwrap();
+        let input = fs::read_to_string("data/2022/04.txt").unwrap().replace("\r", "");
         let result = year2022::day04::day04(&input);
         std::println!("2022 Day 04 Part 1: {}", result.a);
         std::println!("2022 Day 04 Part 2: {}", result.b);
     }
 
     {
-        let input = fs::read_to_string("data/2022/06.txt").unwrap();
+        let input = fs::read_to_string("data/2022/06.txt").unwrap().replace("\r", "");
         let result = year2022::day06::day06(&input);
         std::println!("2022 Day 06 Part 1: {}", result.a);
         std::println!("2022 Day 06 Part 2: {}", result.b);
