63018c444354b7a3ab9e61ddcf882f528fa7a826
David Holmes <dholmes@dholmes.us>
2024-12-01 01:18:34 -0600

    [2024-01] Use sort_unstable()

--- src/year2024/day01.rs
+++ src/year2024/day01.rs
@@ -12,8 +12,8 @@ use adventofcode_rust::aoc::*;
 pub fn day01(input: &str) -> SolutionResult {
     let mut list1 = input.lines().map(|line| { line.split_ascii_whitespace().next().unwrap().parse::<i32>().unwrap() } ).collect_vec();
     let mut list2 = input.lines().map(|line| { line.split_ascii_whitespace().last().unwrap().parse::<i32>().unwrap() } ).collect_vec();
-    list1.sort();
-    list2.sort();
+    list1.sort_unstable();
+    list2.sort_unstable();
 
     let zipped = list1.iter().zip(list2.iter());
     let a: i32 = zipped.map(|(a, b)| (a - b).abs()).sum();
