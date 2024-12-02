27724a374588f11fb50c5e691edab8de531f4de8
David Holmes <dholmes@dholmes.us>
2024-12-02 01:09:51 -0600

    Fix license boilerplate typo

--- src/year2024/day02.rs
+++ src/year2024/day02.rs
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2024 David Holmes (2dholmes at dholmes dot us)
+// Copyright (c) 2024 David Holmes (dholmes at dholmes dot us)
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
