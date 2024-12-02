f3f74135794e08e54952aba55b77fced920c4a08
David Holmes <dholmes@dholmes.us>
2024-12-01 22:54:51 -0600

    [2024-01] Some improvements

--- src/year2024/day01.rs
+++ src/year2024/day01.rs
@@ -1,17 +1,20 @@
 //
-// Copyright (c) 2024 David Holmes (dholmes at dholmes dot us)
+// Copyright (c) 2024 David Holmes (2dholmes at dholmes dot us)
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 
-use std::collections::HashMap;
-use itertools::Itertools;
 use adventofcode_rust::aoc::*;
+use itertools::Itertools;
+use std::collections::HashMap;
 
 pub fn day01(input: &str) -> SolutionResult {
-    let mut list1 = input.lines().map(|line| { line.split_ascii_whitespace().next().unwrap().parse::<i32>().unwrap() } ).collect_vec();
-    let mut list2 = input.lines().map(|line| { line.split_ascii_whitespace().last().unwrap().parse::<i32>().unwrap() } ).collect_vec();
+    let (mut list1, mut list2): (Vec<_>, Vec<_>) = input
+        .split_ascii_whitespace()
+        .map(|num| num.parse::<i32>().unwrap())
+        .tuples::<(_, _)>()
+        .unzip();
     list1.sort_unstable();
     list2.sort_unstable();
 
@@ -19,9 +22,17 @@ pub fn day01(input: &str) -> SolutionResult {
     let a: i32 = zipped.map(|(a, b)| (a - b).abs()).sum();
 
     let mut list2counts = HashMap::new();
-    for i in list2 { *list2counts.entry(i).or_insert(0) += 1; }
+    for i in list2 {
+        *list2counts.entry(i).or_insert(0) += 1;
+    }
 
-    let b: i32 = list1.iter().map(|i| i * *list2counts.entry(*i).or_insert(0)).sum();
+    let b: i32 = list1
+        .iter()
+        .map(|i| i * *list2counts.get(i).unwrap_or(&0))
+        .sum();
 
-    SolutionResult { a: a.to_string(), b: b.to_string()}
+    SolutionResult {
+        a: a.to_string(),
+        b: b.to_string(),
+    }
 }
