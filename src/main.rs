236e0e58df82844cf582ca1708ead5afc24a98f9
David Holmes <dholmes@dholmes.us>
2024-12-02 00:42:12 -0600

    [2024-02] Solution

--- src/main.rs
+++ src/main.rs
@@ -12,47 +12,56 @@ mod year2022;
 mod year2024;
 
 fn main() {
-    {
-        let input = fs::read_to_string("data/2021/01.txt").unwrap().replace("\r", "");
-        let result = year2021::day01::day01(&input);
-        std::println!("2021 Day 01 Part 1: {}", result.a);
-        std::println!("2021 Day 01 Part 2: {}", result.b);
-    }
+    // {
+    //     let input = fs::read_to_string("data/2021/01.txt").unwrap().replace("\r", "");
+    //     let result = year2021::day01::day01(&input);
+    //     std::println!("2021 Day 01 Part 1: {}", result.a);
+    //     std::println!("2021 Day 01 Part 2: {}", result.b);
+    // }
+    //
+    // {
+    //     let input = fs::read_to_string("data/2022/01.txt").unwrap().replace("\r", "");
+    //     let result = year2022::day01::day01(&input);
+    //     std::println!("2022 Day 01 Part 1: {}", result.a);
+    //     std::println!("2022 Day 01 Part 2: {}", result.b);
+    // }
+    //
+    // {
+    //     let input = fs::read_to_string("data/2022/03.txt").unwrap().replace("\r", "");
+    //     let result = year2022::day03::day03(&input);
+    //     std::println!("2022 Day 03 Part 1: {}", result.a);
+    //     std::println!("2022 Day 03 Part 2: {}", result.b);
+    // }
+    //
+    // {
+    //     let input = fs::read_to_string("data/2022/04.txt").unwrap().replace("\r", "");
+    //     let result = year2022::day04::day04(&input);
+    //     std::println!("2022 Day 04 Part 1: {}", result.a);
+    //     std::println!("2022 Day 04 Part 2: {}", result.b);
+    // }
+    //
+    // {
+    //     let input = fs::read_to_string("data/2022/06.txt").unwrap().replace("\r", "");
+    //     let result = year2022::day06::day06(&input);
+    //     std::println!("2022 Day 06 Part 1: {}", result.a);
+    //     std::println!("2022 Day 06 Part 2: {}", result.b);
+    // }
+
+    // {
+    //     let mut input_bytes = fs::read("data/2024/01.txt").unwrap();
+    //     input_bytes.retain(|&x| x != ('\r' as u8));
+    //     let input = std::str::from_utf8(&*input_bytes).unwrap();
+    //     let result = year2024::day01::day01(&input);
+    //     std::println!("2024 Day 01 Part 1: {}", result.a);
+    //     std::println!("2024 Day 01 Part 2: {}", result.b);
+    // }
 
     {
-        let input = fs::read_to_string("data/2022/01.txt").unwrap().replace("\r", "");
-        let result = year2022::day01::day01(&input);
-        std::println!("2022 Day 01 Part 1: {}", result.a);
-        std::println!("2022 Day 01 Part 2: {}", result.b);
-    }
-
-    {
-        let input = fs::read_to_string("data/2022/03.txt").unwrap().replace("\r", "");
-        let result = year2022::day03::day03(&input);
-        std::println!("2022 Day 03 Part 1: {}", result.a);
-        std::println!("2022 Day 03 Part 2: {}", result.b);
-    }
-
-    {
-        let input = fs::read_to_string("data/2022/04.txt").unwrap().replace("\r", "");
-        let result = year2022::day04::day04(&input);
-        std::println!("2022 Day 04 Part 1: {}", result.a);
-        std::println!("2022 Day 04 Part 2: {}", result.b);
-    }
-
-    {
-        let input = fs::read_to_string("data/2022/06.txt").unwrap().replace("\r", "");
-        let result = year2022::day06::day06(&input);
-        std::println!("2022 Day 06 Part 1: {}", result.a);
-        std::println!("2022 Day 06 Part 2: {}", result.b);
-    }
-
-    {
-        let mut input_bytes = fs::read("data/2024/01.txt").unwrap();
+        let mut input_bytes = fs::read("data/2024/02.txt").unwrap();
         input_bytes.retain(|&x| x != ('\r' as u8));
         let input = std::str::from_utf8(&*input_bytes).unwrap();
-        let result = year2024::day01::day01(&input);
-        std::println!("2024 Day 01 Part 1: {}", result.a);
-        std::println!("2024 Day 01 Part 2: {}", result.b);
+        let result = year2024::day02::day02(&input);
+        std::println!("2024 Day 02 Part 1: {}", result.a);
+        std::println!("2024 Day 02 Part 2: {}", result.b);
     }
 }
---
+++ src/year2024/day02.rs
@@ -0,0 +1,50 @@
+//
+// Copyright (c) 2024 David Holmes (2dholmes at dholmes dot us)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+use adventofcode_rust::aoc::SolutionResult;
+use itertools::Itertools;
+
+fn safe<'a, I>(report: I) -> bool
+where
+    I: Sized + Clone + Iterator<Item = &'a i32>,
+{
+    report
+        .clone()
+        .tuple_windows()
+        .map(|(p0, p1)| p0.cmp(&p1))
+        .all_equal()
+        && report.tuple_windows().all(|(p0, p1)| {
+            let diff = p0.abs_diff(*p1);
+            diff >= 1 && diff <= 3
+        })
+}
+
+fn almost_safe(report: &Vec<i32>) -> bool {
+    let mut modified_reports = (0..report.len())
+        .into_iter()
+        .map(|i| report.iter().take(i).chain(report.iter().skip(i + 1)));
+    modified_reports.any(safe)
+}
+
+pub fn day02(input: &str) -> SolutionResult {
+    let reports = input
+        .lines()
+        .map(|l| {
+            l.split_ascii_whitespace()
+                .map(|s| s.parse::<i32>().unwrap())
+                .collect_vec()
+        })
+        .collect_vec();
+
+    let a = reports.iter().filter(|r| safe(r.iter())).count();
+    let b = reports.iter().filter(|r| almost_safe(&r)).count();
+
+    SolutionResult {
+        a: a.to_string(),
+        b: b.to_string(),
+    }
+}
