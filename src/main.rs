1bd94da6af0bb1a1036e5bc361d667971d69df3b
David Holmes <dholmes@dholmes.us>
2024-12-03 20:42:10 -0600

    Remove superfluous std::s

--- src/main.rs
+++ src/main.rs
@@ -15,36 +15,36 @@ fn main() {
     // {
     //     let input = fs::read_to_string("data/2021/01.txt").unwrap().replace("\r", "");
     //     let result = year2021::day01::day01(&input);
-    //     std::println!("2021 Day 01 Part 1: {}", result.a);
-    //     std::println!("2021 Day 01 Part 2: {}", result.b);
+    //     println!("2021 Day 01 Part 1: {}", result.a);
+    //     println!("2021 Day 01 Part 2: {}", result.b);
     // }
     //
     // {
     //     let input = fs::read_to_string("data/2022/01.txt").unwrap().replace("\r", "");
     //     let result = year2022::day01::day01(&input);
-    //     std::println!("2022 Day 01 Part 1: {}", result.a);
-    //     std::println!("2022 Day 01 Part 2: {}", result.b);
+    //     println!("2022 Day 01 Part 1: {}", result.a);
+    //     println!("2022 Day 01 Part 2: {}", result.b);
     // }
     //
     // {
     //     let input = fs::read_to_string("data/2022/03.txt").unwrap().replace("\r", "");
     //     let result = year2022::day03::day03(&input);
-    //     std::println!("2022 Day 03 Part 1: {}", result.a);
-    //     std::println!("2022 Day 03 Part 2: {}", result.b);
+    //     println!("2022 Day 03 Part 1: {}", result.a);
+    //     println!("2022 Day 03 Part 2: {}", result.b);
     // }
     //
     // {
     //     let input = fs::read_to_string("data/2022/04.txt").unwrap().replace("\r", "");
     //     let result = year2022::day04::day04(&input);
-    //     std::println!("2022 Day 04 Part 1: {}", result.a);
-    //     std::println!("2022 Day 04 Part 2: {}", result.b);
+    //     println!("2022 Day 04 Part 1: {}", result.a);
+    //     println!("2022 Day 04 Part 2: {}", result.b);
     // }
     //
     // {
     //     let input = fs::read_to_string("data/2022/06.txt").unwrap().replace("\r", "");
     //     let result = year2022::day06::day06(&input);
-    //     std::println!("2022 Day 06 Part 1: {}", result.a);
-    //     std::println!("2022 Day 06 Part 2: {}", result.b);
+    //     println!("2022 Day 06 Part 1: {}", result.a);
+    //     println!("2022 Day 06 Part 2: {}", result.b);
     // }
 
     // {
@@ -52,8 +52,8 @@ fn main() {
     //     input_bytes.retain(|&x| x != ('\r' as u8));
     //     let input = std::str::from_utf8(&*input_bytes).unwrap();
     //     let result = year2024::day01::day01(&input);
-    //     std::println!("2024 Day 01 Part 1: {}", result.a);
-    //     std::println!("2024 Day 01 Part 2: {}", result.b);
+    //     println!("2024 Day 01 Part 1: {}", result.a);
+    //     println!("2024 Day 01 Part 2: {}", result.b);
     // }
 
     // {
@@ -61,8 +61,8 @@ fn main() {
     //     input_bytes.retain(|&x| x != ('\r' as u8));
     //     let input = std::str::from_utf8(&*input_bytes).unwrap();
     //     let result = year2024::day02::day02(&input);
-    //     std::println!("2024 Day 02 Part 1: {}", result.a);
-    //     std::println!("2024 Day 02 Part 2: {}", result.b);
+    //     println!("2024 Day 02 Part 1: {}", result.a);
+    //     println!("2024 Day 02 Part 2: {}", result.b);
     // }
 
     {
@@ -70,7 +70,7 @@ fn main() {
         input_bytes.retain(|&x| x != ('\r' as u8));
         let input = std::str::from_utf8(&*input_bytes).unwrap();
         let result = year2024::day03::day03(&input);
-        std::println!("2024 Day 03 Part 1: {}", result.a);
-        std::println!("2024 Day 03 Part 2: {}", result.b);
+        println!("2024 Day 03 Part 1: {}", result.a);
+        println!("2024 Day 03 Part 2: {}", result.b);
     }
 }
