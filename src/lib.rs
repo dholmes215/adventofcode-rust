bd8c50abc34b6dda63ed5bd55ed5f1201074c90c
David Holmes <dholmes@dholmes.us>
2024-12-04 22:57:50 -0600

    Grid struct WIP

--- src/lib.rs
+++ src/lib.rs
@@ -19,3 +19,25 @@ impl SolutionResult {
         SolutionResult { a: a.to_string(), b: b.to_string() }
     }
 }
+
+struct Grid<T> {
+    data: Vec<T>,
+    width: usize,
+    height: usize,
+}
+
+impl<T: Default> Grid<T> {
+    pub fn new(width: usize, height: usize) -> Self {
+        let mut grid = Grid{ data: Vec::new(), width, height };
+        grid.data.resize_with(grid.width * grid.height, Default::default);
+        grid
+    }
+    
+    pub fn rows(&self) -> impl Iterator + '_ {
+        self.data.chunks(self.width)
+    }
+    
+    // pub fn cols(&self) -> impl Iterator + '_ {
+    //     (0..self.width).map(|i| )
+    // }
+}
