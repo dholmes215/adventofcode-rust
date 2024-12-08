24df36e5cf734372e1d0dc172663d47535115f1a
David Holmes <dholmes@dholmes.us>
2024-12-08 04:48:34 -0600

    [2024-08] Cleanup & comments

--- src/year2024/day08.rs
+++ src/year2024/day08.rs
@@ -5,30 +5,35 @@
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 
-use std::collections::HashMap;
-use itertools::Itertools;
 use adventofcode_rust::aoc::{Grid, SolutionResult, Vec2};
+use itertools::Itertools;
+use std::collections::HashMap;
 
 pub fn day08(input: &str) -> SolutionResult {
     let mut grid = Grid::from_u8(input.as_bytes());
     let mut antenna_coords: HashMap<u8, Vec<Vec2<isize>>> = HashMap::new();
-    for (x,y) in grid.area().all_points() {
+    for (x, y) in grid.area().all_points() {
         let antenna = grid[(x, y)];
         if antenna.is_ascii_alphanumeric() {
-            antenna_coords.entry(antenna).or_default().push(Vec2 { x, y });
+            antenna_coords
+                .entry(antenna)
+                .or_default()
+                .push(Vec2 { x, y });
         }
     }
-    
+
     let mut grid_b = grid.clone();
     for (_, coords) in &antenna_coords {
-        for (pos1, pos2) in coords.iter().tuple_combinations::<(_,_)>() {
+        for (pos1, pos2) in coords.iter().tuple_combinations::<(_, _)>() {
             let delta = *pos1 - *pos2;
+            // Place part one antinodes in the original grid
             for antinode in [*pos1 + delta, *pos2 - delta] {
                 if grid.area().contains(antinode) {
                     grid[antinode] = b'#';
                 }
             }
-            
+
+            // Place part two antinodes in a second grid
             let mut antinode_b = *pos1;
             while grid.area().contains(antinode_b - delta) {
                 antinode_b -= delta;
@@ -37,12 +42,11 @@ pub fn day08(input: &str) -> SolutionResult {
                 grid_b[antinode_b] = b'#';
                 antinode_b += delta;
             }
-            
         }
     }
-    
+
     let a = grid.data_slice().iter().filter(|&&c| c == b'#').count();
     let b = grid_b.data_slice().iter().filter(|&&c| c == b'#').count();
-    
+
     SolutionResult::new(a, b)
 }
